ff7623b4a3e53c243b5d5fedeb7e79181f9a5248
junbok97 <junbok97@gmail.com>
2022-12-08 00:41:38 +0900

    12/7

---
+++ CodingTest/Lv0.playground/Pages/연속된 수의 합.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ num:Int, _ total:Int) -> [Int] {
+    let startNum = total/num - num/2 + (num % 2 == 0 ? 1 : 0)
+    return (0..<num).map{ $0 + startNum }
+    
+}
+
+solution(5,5)
